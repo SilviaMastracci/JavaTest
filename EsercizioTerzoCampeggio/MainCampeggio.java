a62307bc60892143c6178196d49da01252d57995
PieroMarraffa <pieromarraffa.business@gmail.com>
2023-03-17 11:31:39 +0100

    Update MainCampeggio.java

--- EsercizioTerzoCampeggio/MainCampeggio.java
+++ EsercizioTerzoCampeggio/MainCampeggio.java
@@ -1,9 +1,21 @@
 package EsercizioTerzoCampeggio;
 
+import java.util.ArrayList;
+
 public class MainCampeggio {
     public static void main(String[] args) {
         Cliente cliente = new Cliente("Giovanni", "Marotta", "G.M.@", Genere.Maschio, 33);
+        Staff staff = new Staff(1, "Pippo", "Franco", "pippofranco@gmail.com");
         System.out.println(cliente);
+        System.out.println(staff);
+        ArrayList<Cliente> clienti = new ArrayList<>();
+        clienti.add(cliente);
+
+        ArrayList<Persona> raccoltaFirme = new ArrayList<>();
+        raccoltaFirme.add(cliente);
+        raccoltaFirme.add(staff);
+        //raccoltaFirme.add(new Attività("asd", Genere.Femmina, 23, staff, clienti));
+        System.out.println(raccoltaFirme);
     }
     
 }
